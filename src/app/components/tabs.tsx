9cb6e00e8b5db09e4ac94d2f361586c19fa324b2
giabertu <giabertu99@gmail.com>
2024-02-18 19:32:37 +0000

    fix: added tab iconsg

--- src/app/components/tabs.tsx
+++ src/app/components/tabs.tsx
@@ -2,6 +2,12 @@ import { useRouter } from "next/router"
 import { useSkContext } from "../context/secretKeyContext"
 import { DEFAULT_KEYPAIR } from "../globals"
 import { useState } from "react"
+import { HomeIcon } from "@heroicons/react/24/outline"
+import { UserCircleIcon } from "@heroicons/react/24/outline"
+import { Cog6ToothIcon } from "@heroicons/react/24/outline"
+import { MicrophoneIcon } from "@heroicons/react/24/outline"
+import { ChartBarIcon } from "@heroicons/react/24/outline"
+import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline"
 
 
 enum Tab {
@@ -25,33 +31,34 @@ function Tabs() {
 
   return (
     <div className='w-1/4 debug border border-gray-300 p-2 border-r-0'>
-      <div className="sticky top-20 flex flex-col gap-8 items-center">
-        <button className="border border-gray-300 rounded-3xl p-4"
-        onClick={() => router.push('/home')}
+      <div className="sticky top-20 flex flex-col gap-8 items-start pl-20">
+        <button className="border border-gray-300 rounded-3xl p-4 flex items-center gap-4"
+          onClick={() => router.push('/home')}
         >
-          Home
+          <HomeIcon className="w-6" /> Home
         </button>
-        <button className="border border-gray-300 rounded-3xl p-4">
-          History
+        <button className="border border-gray-300 rounded-3xl p-4 flex items-center gap-4">
+          <ChartBarIcon className="w-6" /> History
         </button>
-        <button className="border border-gray-300 rounded-3xl p-4">
-          Record
+        <button className="border border-gray-300 rounded-3xl p-4 flex items-center gap-4" >
+          <MicrophoneIcon className="w-6" /> Record
         </button>
-        <button className="border border-gray-300 rounded-3xl p-4"
-        onClick={() => router.push('/profile')}
+        <button className="border border-gray-300 rounded-3xl p-4 flex items-center gap-4"
+          onClick={() => router.push('/profile')}
         >
-          Profile
+          <UserCircleIcon className="w-6" /> Profile
         </button>
-        <button className="border border-gray-300 rounded-3xl p-4">
-          Settings
+        <button className="border border-gray-300 rounded-3xl p-4 flex items-center gap-4">
+          <Cog6ToothIcon className="w-6" /> Settings
         </button>
-        <button className="border border-gray-300 rounded-3xl p-4"
+        <button className="border border-gray-300 rounded-3xl p-4 flex items-center gap-4"
           onClick={() => {
             localStorage.removeItem('keyPair')
             setKeyPair(DEFAULT_KEYPAIR)
             router.push('/signin')
 
           }}>
+          <ArrowLeftStartOnRectangleIcon className="w-6" />
           Sign out
         </button>
       </div>
