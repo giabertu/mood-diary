f2193e717427a63cbe9bc480d2126ffe99ee8399
giabertu <giabertu99@gmail.com>
2024-03-13 15:53:39 +0000

    fix: jsx duplication

--- src/app/components/audio/audioRecorder.tsx
+++ src/app/components/audio/audioRecorder.tsx
@@ -1,7 +1,18 @@
 import { useSkContext } from '@/app/context/secretKeyContext';
-import DiaryService from '@/app/services/DiaryService';
+import { hasFailed } from '@/app/globals';
+import DiaryService, { DiaryEntry, ProcessedDiaryEntryResponse } from '@/app/services/DiaryService';
 import { MicrophoneIcon, StopIcon } from '@heroicons/react/24/solid';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { supabase } from '@/app/globals';
+
+const Emotions = [
+  'Angry',
+  'Disgusted',
+  'Fearful',
+  'Happy',
+  'Neutral',
+  'Sad',
+]
 
 
 const AudioRecorder = () => {
@@ -9,11 +20,48 @@ const AudioRecorder = () => {
   const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
   const [audio, setAudio] = useState('');
   const [blob, setBlob] = useState<Blob | null>(null);
+  const [err, setErr] = useState('' as string | null);
+  // const [predictedEmotion, setPredictedEmotion] = useState('' as string | null);
+  const [diaryEntry, setDiaryEntry] = useState<DiaryEntry | null>(null);
+  const [userPredictedEmotion, setUserPredictedEmotion] = useState('' as string | null);
+  const [success, setSuccess] = useState('' as string | null);
+  const [openFeedback, setOpenFeedback] = useState(false);
+  const [hasRecorded, setHasRecorded] = useState(false);
+  // const [recordedEntry, setRecordedEntry] = useState('' as string | null);
 
   const { keyPair, setKeyPair } = useSkContext();
 
 
   const chunks = useRef([] as Blob[])
+
+  useEffect(() => {
+
+    const checkIfRecorded = async () => {
+      //check if the user has already recorded an entry for today
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      const { data, error } = await supabase
+        .from('AudioFiles')
+        .select('*')
+        .gte('created_at', today.toISOString())
+      console.log('data ', data)
+      console.log('error ', error)
+      let typedData = data as DiaryEntry[] | null;
+      if (typedData && typedData.length > 0) {
+        setDiaryEntry(typedData[0]);
+        setHasRecorded(true);
+        const { data: audioUrl, error } = await supabase
+          .storage
+          .from('audio-files')
+          .createSignedUrl(typedData[0].filePath, 60 * 60)
+        console.log("audioUrl ", audioUrl, error)
+        if (audioUrl) {
+          setAudio(audioUrl.signedUrl)
+        }
+      }
+    }
+    checkIfRecorded();
+  }, [])
 
 
   const startRecording = async () => {
@@ -26,8 +74,6 @@ const AudioRecorder = () => {
   };
 
   if (mediaRecorder) {
-
-
     mediaRecorder.ondataavailable = (e: BlobEvent) => {
       console.log('chunk data ', e.data);
       chunks.current.push(e.data)
@@ -45,7 +91,6 @@ const AudioRecorder = () => {
     }
   }
 
-
   const stopRecording = () => {
     if (mediaRecorder) {
       mediaRecorder.stop();
@@ -57,27 +102,74 @@ const AudioRecorder = () => {
 
   const postAudio = async () => {
     if (blob) {
-      const res = await DiaryService.postDiaryEntry(blob, keyPair.npub);
+      const res = await DiaryService.uploadDiaryEntry(blob, keyPair.npub);
       console.log('res ', res);
       if (res.filePath) {
-        const pyRes = await DiaryService.processDiaryEntry(res.filePath);
-        console.log('pyRes ', pyRes);
+        const processedResponse = await DiaryService.processDiaryEntry(res.filePath);
+        console.log('processedEntry', processedResponse);
+        if (hasFailed(processedResponse)) {
+          console.error(processedResponse);
+          setErr(processedResponse.message);
+        } else {
+          setDiaryEntry({ user: keyPair.npub, filePath: processedResponse.filePath, transcript: processedResponse.transcript, modelPredictedEmotion: processedResponse.modelPredictedEmotion });
+          const savedRes = await DiaryService.saveDiaryEntry(processedResponse, keyPair.npub);
+          console.log('savedRes ', savedRes);
+        }
       }
 
     }
   }
 
+  const addFeedback = async (emotion: string) => {
+    const res = await DiaryService.addUserFeedback(emotion, diaryEntry!.filePath);
+    setUserPredictedEmotion(emotion);
+    setSuccess('Thank you for your feedback!')
+    setTimeout(() => {
+      setOpenFeedback(false);
+      setSuccess('');
+    }, 3000);
+  }
+
 
   return (
-    <div className='flex gap-4 '>
-      {!isRecording && !audio && <button className="border border-gray-300 rounded-3xl p-4 flex items-center gap-4"
-        onClick={startRecording}><MicrophoneIcon className='w-10' /></button>}
-      {isRecording && !audio && <button className="border border-gray-300 rounded-3xl p-4 flex items-center gap-4"
-        onClick={stopRecording}><StopIcon className='w-10' /></button>}
-      {audio && <audio src={audio} controls></audio>}
-      {audio && <button onClick={() => setAudio('')}>Delete</button>}
-      {audio && <button onClick={postAudio}>Save</button>}
-    </div>
+    <>
+      <div className='flex flex-col gap-4'>
+        {
+          !hasRecorded ?
+            <div className='flex gap-4 '>
+              {!isRecording && !audio && <button className="border border-gray-300 rounded-3xl p-4 flex items-center gap-4"
+                onClick={startRecording}><MicrophoneIcon className='w-10' /></button>}
+              {isRecording && !audio && <button className="border border-gray-300 rounded-3xl p-4 flex items-center gap-4"
+                onClick={stopRecording}><StopIcon className='w-10' /></button>}
+              {audio && <audio src={audio} controls></audio>}
+              {audio && <button onClick={() => setAudio('')}>Delete</button>}
+              {audio && <button onClick={postAudio}>Save</button>}
+              {err && <p className=' text-red-500'>{err}</p>}
+            </div> :
+            <div>
+              <p>You have already recorded an entry for today.</p>
+              <button onClick={() => setHasRecorded(false)}>Record a new entry</button>
+              {audio && <audio src={audio} controls></audio>}
+            </div>
+        }
+        {diaryEntry &&
+          <div>
+            <p>Your predicted emotion of today is: {diaryEntry.modelPredictedEmotion}.</p>
+            <div className='flex gap-2 '>
+              <p>Not quite right?</p><button onClick={() => setOpenFeedback(true)}>Provide Feedback</button>
+            </div>
+            {openFeedback &&
+              <div>
+                <p>What was your emotion?</p>
+                <div className='flex gap-2'>
+                  {Emotions.filter((e) => e !== diaryEntry.modelPredictedEmotion).map((emotion) => <button key={emotion} onClick={() => addFeedback(emotion)}>{emotion}</button>)}
+                </div>
+                {success && <p className='text-green-500'>{success}</p>}
+              </div>
+            }
+          </div>}
+      </div>
+    </>
   );
 };
 
--- src/app/services/DiaryService.ts
+++ src/app/services/DiaryService.ts
@@ -1,10 +1,28 @@
-import { supabase } from '../globals'
+import { Failed, supabase } from '../globals'
+
+export type ProcessedDiaryEntryResponse = {
+  message: string,
+  modelPredictedEmotion: string,
+  userPredictedEmotion?: string
+  filePath: string,
+  transcript: string
+}
+
+export type DiaryEntry = {
+  user: string,
+  filePath: string,
+  transcript: string,
+  modelPredictedEmotion: string,
+  userPredictedEmotion?: string
+}
+
+
 
 class DiaryService {
 
   constructor() { }
 
-  static async postDiaryEntry(audio: Blob, npub: string): Promise<{ status: string, message: string, filePath?: string }> {
+  static async uploadDiaryEntry(audio: Blob, npub: string): Promise<{ status: string, message: string, filePath?: string }> {
 
     const file = new File([audio], "diary-entry.webm", { type: "audio/webm;codecs=pcm" })
     const filePath = `${npub}/${Date.now()}*${new Date().toLocaleDateString().replace(/\//g, '_')}.webm`;
@@ -27,7 +45,7 @@ class DiaryService {
 
 
 
-  static async processDiaryEntry(filePath: string) {
+  static async processDiaryEntry(filePath: string): Promise<ProcessedDiaryEntryResponse | Failed> {
 
     const response = await fetch('http://127.0.0.1:5000/process_audio', {
       method: 'POST',
@@ -38,6 +56,41 @@ class DiaryService {
     });
 
     return response.json();
+  }
+
+  static async saveDiaryEntry(processedEntry: ProcessedDiaryEntryResponse, npub: string) {
+    const { filePath, transcript, modelPredictedEmotion} = processedEntry;
+    const { data, error } = await supabase
+      .from('AudioFiles')
+      .insert([
+        {
+          user: npub,
+          filePath,
+          transcript,
+          modelPredictedEmotion,
+        }
+      ])
+    if (error) {
+      console.error(error);
+      return { status: 'error', message: 'Failed to save diary entry.' };
+    } else {
+      console.log(data)
+      return { status: 'success', message: 'Diary entry saved successfully.' };
+    }
+  }
+
+  static async addUserFeedback(userPredictedEmotion: string, filePath: string) {
+    const { data, error } = await supabase
+      .from('AudioFiles')
+      .update({ userPredictedEmotion })
+      .eq('filePath', filePath)
+    if (error) {
+      console.error(error);
+      return { status: 'error', message: 'Failed to add user feedback.' };
+    } else {
+      console.log(data)
+      return { status: 'success', message: 'User feedback added successfully.' };
+    }
   }
 }
 
